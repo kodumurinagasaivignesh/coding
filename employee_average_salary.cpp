82ca68eec8885d901e185f4ef56212230200e681
KODUMURI NAGA SAI VIGNESH <kodumurinagasaivignesh@gmail.com>
2026-08-28 11:45:53 +0530

    Add function overloading examples in C++
    Demonstrate function overloading with multiple 'per()' functions for different parameter types and argument counts.

--- employee_average_salary.cpp
+++ employee_average_salary.cpp
@@ -39,3 +39,40 @@ cout <<avg;
 
 
 this is code i want to post in my github reposetory
+
+// **Write a C++ program to demonstrate function overloading by creating multiple `per()` functions with different parameter types and numbers of arguments.**
+
+int per(int a, int b, int c)
+{
+    return a + b + c;
+}
+float per(float a, float b)
+{
+    return a + b;
+}
+int per(int n, int m)
+{
+    return n + m;
+}
+char per(int n, char m)
+{
+    return m;
+}
+
+int main()
+{
+    int a, b;
+    char m, n;
+    float p, r;
+
+    cin >> a >> b >> p >> r >> m >> n;
+
+    cout <<  per(4, 5, 6) << endl;
+
+    cout <<  per(4.5f, 5.7f) << endl;
+
+    cout <<  per(100, 200) << endl;
+
+    cout << per(100, 'a') << endl;
+
+    return 0;
