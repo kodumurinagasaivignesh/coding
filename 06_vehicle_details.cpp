71cd612d6b30950c41235c9031d9bc971f6677b9
KODUMURI NAGA SAI VIGNESH <kodumurinagasaivignesh@gmail.com>
2026-09-15 14:14:21 +0530

    Implement 3D array input/output in main function
    Added a main function to demonstrate 3D array input and output.

--- 06_vehicle_details.cpp
+++ 06_vehicle_details.cpp
@@ -328,3 +328,37 @@ int main()
     ss.dis();
 }
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[2][3][4];
+    int i, j, k;
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            for (k = 0; k < 4; k++)
+            {
+                cin >> a[i][j][k];
+            }
+        }
+    }
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            for (k = 0; k < 4; k++)
+            {
+                cout << a[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
